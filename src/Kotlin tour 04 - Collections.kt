0fbace912ec32262fdd934971787ce28c79d1191
jh <purepleya@gmail.com>
2023-12-10 20:47:05 +0900

    Kotlin tour - Collections 공부 중

---
+++ src/Kotlin tour 04 - Collections.kt
@@ -0,0 +1,8 @@
+fun main() {
+//    Collection types
+//    - Lists : Ordered collections of items
+//    - Sets : Unique unordered collections of items
+//    - Maps : Sets of key-value pairs where keys are unique and map to only one value
+
+
+}
