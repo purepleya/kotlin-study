b5d03e1edf96ded805d535c230e4709d18eb35f0
jh <purepleya@gmail.com>
2023-12-23 12:24:52 +0900

    Basic 01 - Basic syntax

--- src/Basics 01 - Basic syntax.kt
+++ src/Basics 01 - Basic syntax.kt
@@ -106,5 +106,145 @@ class Rectangle2(val height: Double, val width: Double) : Shape("Rectangle") {
  */
 
 
+// String templates
+// 문자 열 안에서 "$변수" 형태나 "${표현식}" 형태로 사용 할 수 있다.
+// "a is $a"
+// "${s1.replace("is", "was")}, but now is $a"
 
 
+// 조건문
+fun maxOf(a: Int, b: Int): Int {
+    if (a > b) {
+        return a
+    } else {
+        return b
+    }
+}
+// 아래처럼 표현식으로 사용 할 수도 있다.
+fun maxOf2(a: Int, b: Int) = if (a > b) a else b
+
+
+
+// for loop
+fun forLoop() {
+    val items = listOf("apple", "banana", "kiwi")
+    for (item in items) {
+        println(item)
+    }
+
+    for (index in items.indices) {
+        println("item at $index is ${items[index]}")
+    }
+
+    for (num in 1..3) {
+        println(num)
+    }
+}
+
+
+// while loop
+fun whileLoop() {
+    val items = listOf("apple", "banana", "kiwi")
+    var index = 0
+    while (index < items.size) {
+        println("item at $index is ${items[index]}")
+        index++
+    }
+}
+
+// when expression
+fun describe(obj: Any): String =
+    when (obj) {
+        1           -> "One"
+        "Hello"     -> "Greeting"
+        is Long     -> "Long"
+        !is String  -> "Not a string"
+        else        -> "Unknown"
+    }
+
+
+// Range
+// range 선언은 1..5 와 같이 .. 연산자를 사용한다. 이는 1, 2, 3, 4, 5 를 포함한다.
+// range 는 in 연산자와 함께 사용 할 수 있다.
+fun range() {
+    val x = 10
+    val y = 9
+    if (x in 1..y+1) {
+        println("fits in range")
+    }
+
+    // !in (not in) 연산자를 사용해서 범위에 포함되지 않는지 검사 할 수 있다.
+    if (x !in 1..y+1) {
+        println("out of range")
+    }
+
+//    step을 이용해서 증가폭을 지정하거나, downTo 로 감소 시킬 수 있다.
+//    1..10 step 2 = [1, 3, 5, 7, 9]
+//    9 downTo 0 step 3 = [9, 6, 3, 0]
+}
+
+
+// Collections
+fun collections() {
+    val items = listOf("apple", "banana", "kiwi")
+
+    // for 문으로 컬렉션을 순회 할 수 있다.
+    for (item in items) {
+        println(item)
+    }
+
+    // in 연산자를 사용해서 컬렉션에 특정 값이 포함되어 있는지 검사 할 수 있다.
+    when {
+        "banana" in items -> println("juicy")
+        "apple" in items -> println("apple is fine too")
+    }
+
+    // 람다식을 사용해서 컬렉션을 filter, map 할 수 있다.
+    items
+        .filter { it.startsWith("a") }
+        .sortedBy { it }
+        .map { it.toUpperCase() }
+        .forEach { println(it) }
+}
+
+
+// Nullable values and null checks
+// null 을 허용하려면 타입 이름 뒤에 ? 를 붙인다.
+// val a: String?
+// fun parseInt(str: String): Int?
+
+// null 값이 가능한 타입의 변수를 사용하려면 null 체크를 해야 한다.
+fun parseInt(str: String): Int? {
+    if (str == "") {
+        return null
+    }
+    return str.toInt()
+}
+
+fun printProduct(arg1: String, arg2: String) {
+    val x = parseInt(arg1)
+    val y = parseInt(arg2)
+
+    // x * y 를 사용하면 컴파일 에러가 발생한다.
+    // 이유는 x 나 y 가 null 일 수 있기 때문이다.
+    if (x != null && y != null) {
+        // null 체크를 통과하면 x 와 y 는 자동으로 non-nullable 로 변환된다.
+        println(x * y)
+    } else {
+        println("'$arg1' or '$arg2' is not a number")
+    }
+}
+
+
+// Type checks and automatic casts
+// is 연산자를 사용해서 타입을 검사 할 수 있다. 이 연산자는 자동으로 타입 변환을 해준다.
+fun getStringLength(obj: Any): Int? {
+    if (obj is String) {
+        // obj 가 자동으로 String 타입으로 변환된다.
+        return obj.length
+    }
+
+    // 타입 검사를 통과하면 자동으로 타입 변환을 해주기 때문에
+    // 아래에서는 obj 를 String 타입으로 사용 할 수 없다.
+    return null
+}
