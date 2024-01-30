58bf8e5be7c15561c2c249bcfe22426b268a0dee
jh <purepleya@gmail.com>
2024-01-30 10:55:41 +0900

    Concepts - Classes and objects (Extensions 공부중...)

--- src/Concepts 04 - Classes and objects (Extensions).kt
+++ src/Concepts 04 - Classes and objects (Extensions).kt
@@ -1,8 +1,23 @@
+fun main() {
+//    usecaseOfSwap()
+
+
+//    val ex = Example()
+//    ex.printFunctionType()
+//    Class method
+//    ex.printFunctionType(2)
+//    Extension function overload : value is 2
+
+    MyClass.printCompanion()
+//    companion
+}
+
 fun <T> MutableList<T>.swap(index1: Int, index2: Int) {
     val tmp = this[index1]
     this[index1] = this[index2]
     this[index2] = tmp
 }
+
 
 fun usecaseOfSwap() {
     val list = mutableListOf(1, 2, 3)
@@ -14,19 +29,28 @@ fun usecaseOfSwap() {
 }
 
 
-fun main() {
-//    usecaseOfSwap()
-    val ex = Example()
-    ex.printFunctionType()
-//    Class method
-    ex.printFunctionType(2)
-//    Extension function overload : value is 2
-}
-
-
 class Example {
     fun printFunctionType() { println("Class method") }
 }
 
 fun Example.printFunctionType() { println("Extension function") }
 fun Example.printFunctionType(i: Int) { println("Extension function overload : value is $i") }
+
+
+fun Any?.toString(): String {
+    if (this == null) return "null"
+    return toString()
+}
+
+// 오류 : initializers are not allowed for extension properties
+//val <T> List<T>.lastIndex: Int = 10
+
+val <T> List<T>.lastIndex: Int
+    get() = size - 1
+
+
+class MyClass {
+    companion object { }  // will be called "Companion"
+}
+
+fun MyClass.Companion.printCompanion() { println("companion") }
