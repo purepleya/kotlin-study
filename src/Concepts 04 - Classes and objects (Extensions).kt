908e40719765adf37719ada07d4988fc032430c0
jh <purepleya@gmail.com>
2024-01-31 09:59:02 +0900

    Concepts - Classes and objects (Extensions)

--- src/Concepts 04 - Classes and objects (Extensions).kt
+++ src/Concepts 04 - Classes and objects (Extensions).kt
@@ -8,8 +8,31 @@ fun main() {
 //    ex.printFunctionType(2)
 //    Extension function overload : value is 2
 
-    MyClass.printCompanion()
+
+//    MyClass.printCompanion()
 //    companion
+
+
+//    val host = Host("kotl.in")
+//    Connection(host, 443).connect()
+//    host.printConnectionString() // 컴파일 에러, 확장 함수는 호스트 클래스 안에서만 사용 가능
+
+
+    ExtensionBaseCaller().call(ExtensionBase())
+//    Base extension function in ExtensionBaseCaller
+    ExtensionBaseCaller().call(ExtensionDerived())
+//    Base extension function in ExtensionBaseCaller
+
+    ExtensionBaseCaller().callDerived(ExtensionDerived())
+//    Derived extension function in ExtensionBaseCaller
+
+    ExtensionDerivedCaller().call(ExtensionBase())
+//    Base extension function in ExtensionDerivedCaller - dispatch receiver is resolved virtually
+    ExtensionDerivedCaller().call(ExtensionDerived())
+//    Base extension function in ExtensionDerivedCaller - extension receiver is resolved statically
+
+    ExtensionDerivedCaller().callDerived(ExtensionDerived())
+//    Derived extension function in ExtensionDerivedCaller
 }
 
 fun <T> MutableList<T>.swap(index1: Int, index2: Int) {
@@ -54,3 +77,61 @@ class MyClass {
 }
 
 fun MyClass.Companion.printCompanion() { println("companion") }
+
+
+
+class Host(val hostname: String) {
+    fun printHostname() { print(hostname) }
+}
+
+class Connection(val host: Host, val port: Int) {
+    fun printPort() { print(port) }
+
+    // Host 클래스가 extension receiver
+    // Connection 클래스가 dispatch receiver
+    fun Host.printConnectionString() {
+        printHostname()
+        print(":")
+        printPort()
+    }
+
+    fun Host.getConnectionString() {
+        toString() // Host의 toString()을 호출
+        this@Connection.toString() // Connection의 toString()을 호출
+    }
+
+    fun connect() {
+        host.printConnectionString()
+    }
+}
+
+open class ExtensionBase {}
+class ExtensionDerived : ExtensionBase() {}
+
+open class ExtensionBaseCaller {
+    open fun ExtensionBase.printFunction() {
+        println("Base extension function in ExtensionBaseCaller")
+    }
+
+    open fun ExtensionDerived.printFunction() {
+        println("Derived extension function in ExtensionBaseCaller")
+    }
+
+    fun call(b: ExtensionBase) {
+        b.printFunction()
+    }
+
+    fun callDerived(b: ExtensionDerived) {
+        b.printFunction()
+    }
+}
+
+class ExtensionDerivedCaller : ExtensionBaseCaller() {
+    override fun ExtensionBase.printFunction() {
+        println("Base extension function in ExtensionDerivedCaller")
+    }
+
+    override fun ExtensionDerived.printFunction() {
+        println("Derived extension function in ExtensionDerivedCaller")
+    }
+}
