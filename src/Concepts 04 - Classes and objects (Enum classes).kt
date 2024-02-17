a5fa4724070a49e22366552e5999ba7bbff7c950
jh <purepleya@gmail.com>
2024-02-17 18:54:07 +0900

    Concepts - Classes and objects (Enum classes)

--- src/Concepts 04 - Classes and objects (Enum classes).kt
+++ src/Concepts 04 - Classes and objects (Enum classes).kt
@@ -25,12 +25,6 @@ enum class ProtocolState {
     abstract fun signal(): ProtocolState
 }
 
-fun main() {
-    println(ProtocolState.WAITING)
-//    WAITING
-    println(ProtocolState.WAITING.signal())
-//    TALKING
-}
 
 open class HelloEnum {
     open fun sayHello() {
@@ -38,6 +32,79 @@ open class HelloEnum {
     }
 }
 
-// class는 상속 받을 수 없음
-enum class ChildEnum: HelloEnum() {
+// compile error - class는 상속 받을 수 없음
+//enum class ChildEnum: HelloEnum() {
+//}
+
+interface HelloInterface {
+    fun sayHello()
+}
+
+enum class ChildEnum2: HelloInterface {
+    A {
+        override fun sayHello() {
+            println("This is A")
+        }
+    },
+    B {
+        override fun sayHello2() {
+            println("This is B")
+        }
+    },
+    C;
+
+    override fun sayHello() {
+        println("This is Common")
+        sayHello2()
+    }
+
+    open fun sayHello2() {
+        println("This is Common2")
+    }
+}
+
+
+
+
+
+fun main() {
+//    ChildEnum2.A.sayHello()
+//    This is A
+
+//    ChildEnum2.B.sayHello()
+//    This is Common
+//    This is B
+
+//    ChildEnum2.C.sayHello()
+//    This is Common
+//    This is Common2
+
+
+    for (e in ChildEnum2.entries) {
+        println("name : ${e.name}, ordinal : ${e.ordinal}")
+    }
+//    name : A, ordinal : 0
+//    name : B, ordinal : 1
+//    name : C, ordinal : 2
+
+    val a = ChildEnum2.valueOf("A")
+    println(a)
+//    A
+    val b = ChildEnum2.valueOf("B")
+    println(b)
+//    B
+
+
+    for (e in enumValues<ChildEnum2>()) {
+        println("name : ${e.name}, ordinal : ${e.ordinal}")
+    }
+//    name : A, ordinal : 0
+//    name : B, ordinal : 1
+//    name : C, ordinal : 2
+
+    val a2 = enumValueOf<ChildEnum2>("A")
+    println(a2)
+//    A
+
+
 }
