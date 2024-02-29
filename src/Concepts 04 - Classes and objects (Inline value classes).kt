61a89346e07162e72584587af8ae455ef0e6054a
jh <purepleya@gmail.com>
2024-02-29 09:12:07 +0900

    Concepts 04 - Classes and objects (Inline value classes) 공부 중

--- src/Concepts 04 - Classes and objects (Inline value classes).kt
+++ src/Concepts 04 - Classes and objects (Inline value classes).kt
@@ -21,14 +21,28 @@ value class PersonInline(private val fullName: String) {
 }
 
 
+interface Printable {
+    fun prettyPrint(): String
+}
+
+@JvmInline
+value class Name(val s: String) : Printable {
+    override fun prettyPrint(): String = "Let's $s!"
+}
+
+
+
 
 fun main() {
-    val name1 = PersonInline("Kotlin", "Mascot")
-    val name2 = PersonInline("Kodee")
+//    val name1 = PersonInline("Kotlin", "Mascot")
+//    val name2 = PersonInline("Kodee")
+//
+//    name1.greet()
+//    // Hello, Kotlin Mascot
+//
+//    println(name2.length)
+//    // 5
 
-    name1.greet()
-    // Hello, Kotlin Mascot
-
-    println(name2.length)
-    // 5
+    val name = Name("Kotlin")
+    println(name.prettyPrint())
 }
