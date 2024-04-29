4f0455e12c4906effc3162a200d63375220e9b52
jh <purepleya@gmail.com>
2024-04-29 10:07:45 +0900

    coroutine - asynchronous flow... zip, combine, flatMap

--- src/Coroutine - Asyncronous Flow.kt
+++ src/Coroutine - Asyncronous Flow.kt
@@ -39,6 +39,12 @@ fun numbers(): Flow<Int> = flow {
     }
 }
 
+fun requestFlow(i: Int): Flow<String> = flow {
+    emit("$i: First")
+    delay(500)
+    emit("$i: Second")
+}
+
 
 fun main() {
 //    simple().forEach { value -> println(value) }
@@ -81,15 +87,34 @@ fun main() {
 //                println("${LocalDateTime.now()} [${Thread.currentThread().name}, ${currentCoroutineContext()}] Collected $value")
 //            }
 
-        simpleFlow()
-            .collectLatest { value ->
-                try {
-                    println("${LocalDateTime.now()} [${Thread.currentThread().name}, ${currentCoroutineContext()}] Collected $value")
-                    delay(300)
-                    println("${LocalDateTime.now()} [${Thread.currentThread().name}, ${currentCoroutineContext()}] Done $value")
-                } catch (e: Exception) {
-                    println("${LocalDateTime.now()} [${Thread.currentThread().name}, ${currentCoroutineContext()}] Exception $e")
-                }
+//        simpleFlow()
+//            .collectLatest { value ->
+//                try {
+//                    println("${LocalDateTime.now()} [${Thread.currentThread().name}, ${currentCoroutineContext()}] Collected $value")
+//                    delay(300)
+//                    println("${LocalDateTime.now()} [${Thread.currentThread().name}, ${currentCoroutineContext()}] Done $value")
+//                } catch (e: Exception) {
+//                    println("${LocalDateTime.now()} [${Thread.currentThread().name}, ${currentCoroutineContext()}] Exception $e")
+//                }
+//            }
+
+
+//        val nums = (1..4).asFlow().onEach { delay(300) }
+//        val strs = flowOf("one", "two", "three").onEach { delay(400) }
+//        val startTime = System.currentTimeMillis()
+//        nums
+////            .zip(strs) { a, b -> "$a -> $b" }
+//            .combine(strs) { a, b -> "$a -> $b" }
+//            .collect { value ->
+//                println("$value at ${System.currentTimeMillis() - startTime} ms from start")
+//            }
+
+        val startTime = System.currentTimeMillis()
+        (1..3).asFlow().onEach { delay(600) }
+//            .flatMapConcat { requestFlow(it) }
+            .flatMapMerge { requestFlow(it) }
+            .collect { response ->
+                println("$response at ${System.currentTimeMillis() - startTime} ms from start")
             }
 
     }
